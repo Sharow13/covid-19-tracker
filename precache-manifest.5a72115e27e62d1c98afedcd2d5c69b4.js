e243c6b58a9951ebd054223bb2d275657666118c
Zsók Győző <zsokgyozo@Zsoks-Mac-mini.local>
2020-06-01 19:27:46 +0200

    Updates

---
+++ precache-manifest.5a72115e27e62d1c98afedcd2d5c69b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9854c0e01c9894f479ad83c03e4486",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "92ade59b788aa5296eff",
+    "url": "/covid-19-tracker/static/css/main.edfa8693.chunk.css"
+  },
+  {
+    "revision": "ec1b26db1b14f2f409b0",
+    "url": "/covid-19-tracker/static/js/2.5f83476c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.5f83476c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ade59b788aa5296eff",
+    "url": "/covid-19-tracker/static/js/main.c5292b99.chunk.js"
+  },
+  {
+    "revision": "d01ffe69296e18bcc217",
+    "url": "/covid-19-tracker/static/js/runtime-main.9aee7480.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
